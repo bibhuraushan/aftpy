2fe77af907f9a7e49df16013ef242a2cc450a8f7
bibhuraushan <bibhuraushan1@gmail.com>
2024-02-08 12:05:47 -0700

    aftmap module created.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aftpy',
+    version='0.0.1',
+    packages=['aftpy'],
+    url='',
+    license='',
+    author='Bibhuti Kumar Jha',
+    author_email='bibhuraushan1@gmail.com',
+    description='Python package to read and download AFTmap data.'
+)
